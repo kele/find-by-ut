48b2bc1e194e9bce84c6ce7278c6f74f27d0bbc5
Damian Bogel <kele@google.com>
2015-07-20 16:25:07 -0400

    DatastoreGuido now adds an entry iff it's unique.

--- guido/datastore_guido.py
+++ guido/datastore_guido.py
@@ -14,16 +14,22 @@ class FunctionMetadata(ndb.Model):
 
 class DatastoreGuido(guido.Guido):
   def add_function(self, name, args, def_args, filepath, location):
+    q = FunctionMetadata.query(FunctionMetadata.name == name)
+    q = q.filter(FunctionMetadata.filepath == filepath).get()
+    if q:
+      return False
+
     f = FunctionMetadata(
         name=name,
         args=args,
         num_of_args=len(args),
         def_args=def_args,
         num_of_def_args=len(def_args),
-        filepath=filepath
+        filepath=filepath,
         location_start=location[0],
         location_end=location[1])
     f.put()
+    return True
 
   def search(self, num_args):
     q = FunctionMetadata.query(FunctionMetadata.num_of_args <= num_args)
