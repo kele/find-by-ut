e575c6129923e764491d5b91237cdabf57da5116
Damian Bogel <kele@google.com>
2015-07-20 14:24:47 -0400

    Added simple PythonRunner.

---
+++ runner/python_runner.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python2
+
+from runner import Runner
+import imp
+
+class PythonRunner(Runner):
+  def get_supported_languages():
+    return ["python"]
+
+  def run(self, test, env):
+    ready_to_run = self._fill_test(test, env["filename"], env["name"])
+    try:
+      exec(ready_to_run)
+      return True
+    except:
+      raise
+
+
+  @staticmethod
+  def _fill_test(test, filepath, function_name):
+    return """
+FUNCTION = imp.load_source("imported_modulename", "{filepath}").{function_name}
+""".format(filepath=filepath, function_name=function_name) + test
+
+
