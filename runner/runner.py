a96b41fe923f7aef466119fdeae70376cbeb1af1
Damian Bogel <kele@google.com>
2015-07-20 12:45:31 -0400

    Added Runner interface.

---
+++ runner/runner.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python2
+
+class Runner:
+  def get_supported_languages():
+    """
+    Returns:
+      [string]: list of supported languages
+    """
+
+    return []
+
+  def run(self, test, env):
+    """
+    Execute the test in a specified environment.
+
+    Args:
+      test (string): the test code
+      env (implementation specific): environment to run the test
+
+    Returns:
+      bool: result of the test
+    """
+
+    raise NotImplementedError
+
+
+
