75779c61c4f6c8736db6b61fd59fee271e0ba325
Damian Bogel <kele@google.com>
2015-07-20 16:25:37 -0400

    Changed main.py just for testing purposes.

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+from guido.datastore_guido import DatastoreGuido
+
+dg = DatastoreGuido()
+
 @app.route('/')
 def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    dg.add_function("lol", [], [], "file.py", (5, 6))
+    dg.add_function("lol2", ['x'], [], "file.py", (8, 19))
+    res = dg.search(1)
+    return "Results:<br>" + '<br>'.join([f.name for f in res]) + "<br>Results:<br>" + '<br>'.join([f.name for f in dg.search(0)])
+
