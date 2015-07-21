ec0c443de1c6e366a12684d2f5d403bbee866366
Damian Bogel <kele@google.com>
2015-07-21 14:58:45 -0400

    PythonRunner now supports run_bulk()

--- runner/python_runner.py
+++ runner/python_runner.py
@@ -5,6 +5,7 @@ import imp
 import os
 import re
 import sys
+import compiler
 
 from guido import Guido
 
@@ -21,13 +22,34 @@ class PythonRunner(Runner):
       exec(ready_to_run)
       return True
     except:
-      print "Unexpected error: " sys.exec_info()[0]
+      print "Unexpected error: " + sys.exec_info()[0]
       return False
 
-  def run_bulk(self, test_body, num_args, file_regex):
+  def run_bulk(self, test_body, file_regex):
+    num_args = self._get_num_args(test_body)
+    if num_args < 0:
+      raise ValueError
+
     functions = _GUIDO.search(num_args, file_regex)
     good = [f in functions if self.run(test_body, { "filepath" : f.filepath, "name" : f.name })]
     return good
+
+
+  @staticmethod
+  def _get_num_args(test_body):
+
+    class FunctionVisitor(compiler.visitor.ASTVisitor):
+      def __init__(self):
+        self.num_args = -1
+
+      def visitCallFunc(self, node):
+        if node.node.name == "FUNCTION":
+          self.num_args = len(node.args)
+
+    ast = compiler.parse(test_body)
+    visitor = CallVisitor()
+    compiler.walk(ast, visitor)
+    return visitor.num_args
 
   @staticmethod
   def _fill_test(test, filepath, function_name):
