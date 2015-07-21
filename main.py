e150ce003f18cfe8abbac18220c49137c67a3851
Damian Bogel <kele@google.com>
2015-07-21 16:40:47 -0400

    Bugfixing! <3

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 from flask import Flask, Markup, render_template, request, url_for
-from google.appengine.api import taskqueue, Queue, Task
-from guido.datastore_guido import DatastoreGuido
-from scanner.scanner import scan_config
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route('/work', methods=['POST'])
-def work():
-    return 'helloworld'
 
-@app.route('/ingest')
+@app.route('/scan')
 def ingest():
   from scanner.scanner import main as main_scanner
   main_scanner()
@@ -19,9 +13,7 @@ def ingest():
 def run(code):
   from runner.python_runner import PythonRunner
   r = PythonRunner()
-
   good = r.run_bulk(code, ".*")
-
   return '<br>'.join([g.name + " @ " + g.filepath for g in good])
 
 @app.route('/frontend')
