066f85c7df8ad2a36ba1ab8df8067f1f230a4e46
Damian Bogel <kele@google.com>
2015-07-21 16:29:31 -0400

    main.py cleanup

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
-from flask import Flask, Markup
-app = Flask(__name__)
-app.config['DEBUG'] = True
-from google.appengine.api.taskqueue import Queue, Task
-from google.appengine.api import taskqueue
-
-from flask import render_template, request, url_for
-
+from flask import Flask, Markup, render_template, request, url_for
+from google.appengine.api import taskqueue, Queue, Task
 from guido.datastore_guido import DatastoreGuido
 from scanner.scanner import scan_config
 
-dg = DatastoreGuido()
-
-def get_results(*args, **kwargs):
-  return [f.name + "(" + ', '.join(f.args) + ")" for f in dg.search(*args, **kwargs)]
-
-@app.route('/push')
-def push():
-    q = Queue()
-    task = Task(url='/work')
-    rpc = q.add_async(task)
-    return str(rpc.get_result())
-    # taskqueue.add(url='/work')
-    # return 'meow'
+app = Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route('/work', methods=['POST'])
 def work():
@@ -32,22 +15,6 @@ def ingest():
   from scanner.scanner import main as main_scanner
   main_scanner()
   return "Done!"
-
-@app.route('/')
-def hello():
-    dg.add_lazy("lol", [], [], "file.py", 5)
-    dg.add_lazy("lol1", ['x'], [], "file.py", 10)
-    dg.add_lazy("lol2", ['y'], [], "/this/funny/dir/file.py", 10)
-    dg.add_lazy("lol3", ['z'], [], "/this/funny/dir/file.py", 10)
-    dg.add_lazy("lol4", ['q'], [], "/this/notfunny/dir/file.py", 10)
-    dg.flush()
-
-
-    res1 = '<br>'.join(get_results(num_args=0))
-    res2 = '<br>'.join(get_results(num_args=1, file_regex=".*this.*"))
-    res3 = '<br>'.join(get_results(num_args=1, file_regex=".*notfunny.*"))
-    res = '<hr>'.join([res1, res2, res3])
-    return res
 
 def run(code):
   from runner.python_runner import PythonRunner
