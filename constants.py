0a9faef1ca3e435e88c71b4947d83c36f67fbf5f
Damian Bogel <kele@google.com>
2015-07-22 09:47:17 -0400

    frontend improv

--- constants.py
+++ constants.py
@@ -7,4 +7,4 @@ RESULT_KEYNAME = 'RESULT'
 SERVER_SOFTWARE = os.environ.get('SERVER_SOFTWARE')
 IS_PRODUCTION = SERVER_SOFTWARE.startswith('Google')
 DEFAULT_VERSION_HOSTNAME = get_default_version_hostname()
-HOST = get_default_version_hostname() if IS_PRODUCTION else 'localhost:8080'
+HOST = 'hackaton-1009.appspot.com' if IS_PRODUCTION else 'localhost:8080'
